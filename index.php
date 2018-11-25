2f12e5db09f6c5f9e7d2dd5ccbd7eef07f68bcbb
TImur <timofejr064@gmail.com>
2018-11-25 11:59:39 +0500

    Made some changes

---
+++ index.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include "header.php";  ?>
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Тест.ру</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Домой</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+<div class="row">
+    <div class="col text-center">
+        <h3 class="test">Тест по безопасности в сети</h3>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <!-- Вопрос 1 !-->
+            <div class="testframe">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 1</h1>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <button id="btn1" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 2 !-->
+            <div class="testframe2">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 2</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn2" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 3 !-->
+            <div class="testframe3">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 3</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn3" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 4 !-->
+            <div class="testframe4">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 4</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn4" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 5 !-->
+            <div class="testframe5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 5</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn5" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 6 !-->
+            <div class="testframe6">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 6</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn6" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 7 !-->
+            <div class="testframe7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 7</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn7" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 8 !-->
+            <div class="testframe8">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 8</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn8" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 9 !-->
+            <div class="testframe9">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 9</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn9" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+            <!-- Вопрос 10 !-->
+            <div class="testframe10">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3">
+                        <h1>Вопрос 10</h1>
+                        <input type="radio" name="rad1">
+                        <b>Какой-то вопрос<br>
+                            <input type="radio" name="rad1">
+                            <b>Какой-то вопрос<br>
+                                <input type="radio" name="rad1">
+                                <b>Какой-то вопрос<br>
+                                    <button id="btn10" class="btn btn-success">Продолжить</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<script>
+    //вопрос 1
+    $('#btn1').on('click',function () {
+        $('.testframe2').show('100');
+        $('.testframe').hide('100');
+    });
+    //вопрос 2
+    $('#btn2').on('click',function () {
+        $('.testframe3').show('100');
+        $('.testframe2').hide('100');
+    });
+    //вопрос 3
+    $('#btn3').on('click',function () {
+        $('.testframe4').show('100');
+        $('.testframe3').hide('100');
+    });
+    //вопрос 1
+    $('#btn4').on('click',function () {
+        $('.testframe5').show('100');
+        $('.testframe4').hide('100');
+    });
+    //вопрос 1
+    $('#btn5').on('click',function () {
+        $('.testframe6').show('100');
+        $('.testframe5').hide('100');
+    });
+    //вопрос 1
+    $('#btn6').on('click',function () {
+        $('.testframe7').show('100');
+        $('.testframe6').hide('100');
+    });
+    //вопрос 1
+    $('#btn7').on('click',function () {
+        $('.testframe8').show('100');
+        $('.testframe7').hide('100');
+    });
+    //вопрос 1
+    $('#btn8').on('click',function () {
+        $('.testframe9').show('100');
+        $('.testframe8').hide('100');
+    });
+    //вопрос 1
+    $('#btn9').on('click',function () {
+        $('.testframe10').show('100');
+        $('.testframe9').hide('100');
+    });
+    //вопрос 1
+    $('#btn10').on('click',function () {
+        $('.testframe10').show('100');
+        $('.testframe').hide('100');
+    });
+</script>
+</html>
